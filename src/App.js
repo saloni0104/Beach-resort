2e00788faad56f9124dc11c46d6aa0adfb521e6a
Saloni <suparekh01@gmail.com>
2020-05-08 02:05:06 +0530

    Router set up

--- src/App.js
+++ src/App.js
@@ -7,13 +7,19 @@ import Rooms from './pages/Rooms';
 import SingleRoom from './pages/SingleRoom';
 import Error from './pages/Error';
 
+import {Route, Switch} from 'react-router-dom';
+
 function App() {
   return (
     <> 
-    <Home></Home>
-    <Rooms></Rooms>
-    <SingleRoom></SingleRoom>
-    <Error></Error>
+    <Switch>
+    <Route exact path="/" component={Home} />
+    <Route exact path="/rooms/" component={Rooms} />
+    <Route exact path="/rooms/:slug" component={SingleRoom} />
+    <Route component={Error} />
+    </Switch>
+    
+    
     </>
   );
 }
