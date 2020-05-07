75144cd375129b9899883c3f5bbae95c04a8ae5c
Saloni <suparekh01@gmail.com>
2020-05-08 02:58:57 +0530

    Hero component added

--- src/pages/Rooms.js
+++ src/pages/Rooms.js
@@ -1,9 +1,6 @@
-import React from 'react'
+import React from 'react';
+import Hero from '../components/Hero';
 
 export default function Rooms() {
-    return (
-        <div>
-            Hello from Rooms page
-        </div>
-    )
-}
+    return <Hero hero= "roomsHero" />;
+};
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from 'react';
+import Hero from "../components/Hero";
 
 export default function home() {
-    return (
-        <div>
-            hello from home page
-        </div>
-    )
+    return <Hero />;
+    
 }
