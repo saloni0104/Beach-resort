2b405e5215a12db43fe06e85c5759319ce6265d3
Saloni <suparekh01@gmail.com>
2020-05-09 04:40:05 +0530

    Single Rooms page Complete

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react'
 import defaultBcg from '../images/room-1.jpeg';
 import Hero from '../components/Hero';
 import Banner from '../components/Banner';
-import {Link} from 'react-router-dom';
-import {RoomContext} from '../context';
+import { Link } from 'react-router-dom';
+import { RoomContext } from '../context';
 import StyledHero from '../components/StyledHero';
 
 
@@ -12,7 +12,7 @@ export default class SingleRoom extends Component {
     constructor(props) {
         super(props);
         //console.log(this.props);
-        this.state ={
+        this.state = {
             slug: this.props.match.params.slug,
             defaultBcg
         };
@@ -22,28 +22,66 @@ export default class SingleRoom extends Component {
     //componentDidMount() {}
 
     render() {
-        const {getRoom} = this.context;
+        const { getRoom } = this.context;
         const room = getRoom(this.state.slug);
-        if(!room) {
-            return(
+        if (!room) {
+            return (
                 <div className="error">
-                <h3>No such Room could be found...</h3>
-                <Link to='/rooms' className='btn-primary'>
-                    Back to rooms
+                    <h3>No such Room could be found...</h3>
+                    <Link to='/rooms' className='btn-primary'>
+                        Back to rooms
                 </Link>
-            </div>
+                </div>
             );
         }
-        const {name, description, capacity, size, price, extras, breakfast, pets, images } =room
-
+        const { name, description, capacity, size, price, extras, breakfast, pets, images } = room
+        const [mainImg, ...defaultImg] = images; //to get three images in single room page, excluding the hero image
+        //this is destructuring of array with mainImg as first image and defaultImg as the remaining elements
         return (
-            <StyledHero img={images[0]}>
-                <Banner title={`${name} room`}>
-                    <Link to='/rooms' className='btn-primary'>
-                        Back to Rooms
+            <>
+                <StyledHero img={mainImg || this.state.defaultBcg}>
+                    <Banner title={`${name} room`}>
+                        <Link to='/rooms' className='btn-primary'>
+                            Back to Rooms
                     </Link>
-                </Banner>
-            </StyledHero>
+                    </Banner>
+                </StyledHero>
+
+                <section className="single-room">
+                    <div className="single-room-images">
+                        {defaultImg.map((item, index) => {
+                            return <img key={index} src={item} alt={name} />
+                        })}
+                    </div>
+                    <div className="single-room-info">
+                        <article className="desc">
+                            <h3>details</h3>
+                            <p>{description}</p>
+                        </article>
+                        <article className="info">
+                            <h3>info</h3>
+                            <h6>price: ${price}</h6>
+                            <h6>size: ${size} SQFT</h6>
+                            <h6>
+                                max capacity: {" "}
+                                    {capacity > 1 ? `${capacity} people` : `${capacity} person` }
+                                
+                            </h6>
+                            <h6>{pets? "pets allowed" : "no pets allowed" }</h6>
+                            <h6>{breakfast && "free breakfast included"}</h6>
+                        </article>
+
+                    </div>
+                </section>
+                <section className="room-extras">
+                    <h6>extras</h6>
+                    <ul className="extras">
+                        {extras.map((item,index) => {
+                            return <li key={index}>-{item}</li>
+                        })}
+                    </ul>
+                </section>
+            </>
         )
     }
 }
