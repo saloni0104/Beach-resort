05ee392df50077ea5c473386dbee45dac1e72289
Saloni <suparekh01@gmail.com>
2020-05-09 05:10:15 +0530

    Room Container completed

--- src/components/RoomsContainer.js
+++ src/components/RoomsContainer.js
@@ -1,6 +1,39 @@
 import React from 'react';
 import RoomsFilter from './RoomsFilter';
 import RoomsList from './RoomsList';
+import { withRoomConsumer } from '../context';
+import Loading from './Loading';
+
+
+function RoomsContainer({ context }) {
+    const { loading, sortedRooms, rooms } = context;
+    if (loading) {
+        return <Loading />;
+    }
+    return (
+        <div>
+           <>
+            <RoomsFilter rooms={rooms} />
+            <RoomsList rooms={sortedRooms} />
+            </>
+        </div>
+    );
+}
+
+export default withRoomConsumer(RoomsContainer)
+
+
+
+
+
+
+
+
+
+
+/*import React from 'react';
+import RoomsFilter from './RoomsFilter';
+import RoomsList from './RoomsList';
 import { RoomConsumer } from '../context';
 import Loading from './Loading';
 
@@ -25,4 +58,4 @@ export default function RoomsContainer() {
         </RoomConsumer>
 
     );
-}
+}*/
--- src/context.js
+++ src/context.js
@@ -56,4 +56,12 @@ const RoomContext = React.createContext();
 
 const RoomConsumer = RoomContext.Consumer;
 
+export function withRoomConsumer(Component) {
+    return function ConsumerWrapper(props) {
+        return <RoomConsumer>
+          {value => <Component {...props} context={value} />}  
+        </RoomConsumer>
+    }
+}
+
 export {RoomProvider, RoomConsumer, RoomContext};
