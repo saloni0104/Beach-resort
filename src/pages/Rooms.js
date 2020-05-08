07d1a54c6f4e4b67dc4cb649fc3f569582d68499
Saloni <suparekh01@gmail.com>
2020-05-08 17:19:54 +0530

    Added services

--- src/pages/Rooms.js
+++ src/pages/Rooms.js
@@ -1,6 +1,18 @@
 import React from 'react';
 import Hero from '../components/Hero';
+import Banner from '../components/Banner';
+import {Link} from 'react-router-dom';
 
 export default function Rooms() {
-    return <Hero hero= "roomsHero" />;
+    return (
+        <Hero hero= "roomsHero">
+            <Banner title="Our Rooms">
+                <Link to='/rooms' className="btn-primary">
+                    Return Home
+        </Link>
+            </Banner>
+        </Hero>
+
+    );
+
 };
--- src/pages/home.js
+++ src/pages/home.js
@@ -2,9 +2,11 @@ import React from 'react';
 import Hero from "../components/Hero";
 import Banner from '../components/Banner';
 import {Link} from 'react-router-dom';
+import Services from '../components/Services';
 
 export default function home() {
     return (
+        <>
         <Hero>
             <Banner title="luxurious rooms" subtitle="deluxe rooms starting at $299">
                 <Link to='/rooms' className="btn-primary">
@@ -12,6 +14,8 @@ export default function home() {
         </Link>
             </Banner>
         </Hero>
+        <Services />
+        </>
 
     );
 }
