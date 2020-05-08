9960e6bd65b8161f275bcbdbad5bb38e0988643c
Saloni <suparekh01@gmail.com>
2020-05-08 18:31:42 +0530

    Room provider accessed

--- src/index.js
+++ src/index.js
@@ -2,14 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 //import './index.css';
 
-import {BrowserRouter as Router} from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import { RoomProvider } from './context';
+
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
+  <RoomProvider>
+    <Router>
+      <App />
+    </Router>
+  </RoomProvider>,
+
   document.getElementById('root')
 );
 
