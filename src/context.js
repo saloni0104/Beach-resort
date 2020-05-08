924c72006c74e6acd5803e3a49737b07a2ae1bec
Saloni <suparekh01@gmail.com>
2020-05-08 19:20:04 +0530

    Room details fetched

--- src/context.js
+++ src/context.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import items from './data';
 
 
 const RoomContext = React.createContext();
@@ -6,11 +7,37 @@ const RoomContext = React.createContext();
 
  class RoomProvider extends Component {
     state = {
-
+        rooms: [],
+        sortedRooms:[],
+        featuredRooms:[],
+        loading:true
     };
+
+    //getData
+
+
+    componentDidMount() {
+        let rooms= this.formatData(items);
+        console.log(rooms);
+
+    }
+
+    formatData(items) {
+        let tempItems = items.map(item => {
+            let id = item.sys.id;
+            let images= item.fields.images.map(image => image.fields.file.url);
+
+            let room={...item.fields,images, id}
+            return room;
+        });
+        return tempItems;
+    }
+
+
+
     render() {
         return (
-            <RoomContext.Provider value="hello">
+            <RoomContext.Provider value={{}}>
                 {this.props.children}
             </RoomContext.Provider>
         );
