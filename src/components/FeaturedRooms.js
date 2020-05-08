7654a6e8f38be5f66860b0f80cc4b6a4a3b03302
Saloni <suparekh01@gmail.com>
2020-05-08 20:09:56 +0530

    Loading element added

--- src/components/FeaturedRooms.js
+++ src/components/FeaturedRooms.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import {RoomContext} from '../context';
+import Loading from "./Loading";
 
 export default class FeaturedRooms extends Component {
     static contextType = RoomContext
@@ -10,6 +11,7 @@ export default class FeaturedRooms extends Component {
         return (
             <div>
                 hello from featured rooms 
+                <Loading />
             </div>
         )
     }
