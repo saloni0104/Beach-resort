ddd0d749893224738e81c234c1abba796385ae78
Saloni <suparekh01@gmail.com>
2020-05-08 17:07:08 +0530

    Banner added

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,7 +1,18 @@
 import React from 'react'
 import Hero from "../components/Hero";
+import Banner from '../components/Banner';
+import { Link } from 'react-router-dom';
 
 export default function Error() {
-    return <Hero />;
-    
+    return (
+        <Hero>
+            <Banner title="404" subtitle="page not found">
+                <Link to='/' className="btn-primary">
+                    our rooms
+        </Link>
+            </Banner>
+        </Hero>
+
+    );
+
 }
