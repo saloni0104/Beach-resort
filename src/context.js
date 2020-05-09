fcff6a1dbdf508dfa78d99e368ca0257135f30c7
Saloni <suparekh01@gmail.com>
2020-05-09 16:58:07 +0530

    Guest filtering done

--- src/context.js
+++ src/context.js
@@ -71,13 +71,26 @@ const RoomContext = React.createContext();
     filterRooms = () => {
         let{
             rooms,type,capacity,price,minSize,maxSize,breakfast,pets
-        } = this.state
+        } = this.state;
+
+        //all the rooms
 
         let tempRooms = [...rooms];
+        // transform the values
+        capacity = parseInt(capacity)
+
+
+        //filter by type
         if(type !== 'all') {
-            tempRooms = tempRooms.filter(room => room.type === type)
+            tempRooms = tempRooms.filter(room => room.type === type);
         }
 
+        // filter by capacity
+        if(capacity !==1){
+            tempRooms = tempRooms.filter(room => room.capacity >= capacity);
+        }
+
+        
         this.setState({
             sortedRooms: tempRooms
         })
